901ab4d6c8313c8e72989681898de481a042a7a6
chenxu31 <chenxu31@gmail.com>
2023-03-17 16:55:46 +0800

    sync

--- evaluate_pelvic_ae.py
+++ evaluate_pelvic_ae.py
@@ -31,8 +31,9 @@ import common_net_pt as common_net
 class ConvViT(vision_transformer.ConvViT):
     """ Vision Transformer with support for global average pooling
     """
-    def __init__(self, decoder_embed_dim, decoder_num_heads, mlp_ratio, norm_layer, in_chans=1, clamp_out=False, **kwargs):
-        super(ConvViT, self).__init__(**kwargs)
+    def __init__(self, decoder_embed_dim, decoder_num_heads, mlp_ratio, norm_layer, decoder_depth, in_chans=1, clamp_out=False, **kwargs):
+        super(ConvViT, self).__init__(decoder_embed_dim=decoder_embed_dim, decoder_num_heads=decoder_num_heads, decoder_depth=decoder_depth,
+                                      mlp_ratio=mlp_ratio, norm_layer=norm_layer, in_chans=in_chans, clamp_out=clamp_out, **kwargs)
         self.clamp_out = clamp_out
         self.decoder_embed = nn.Linear(embed_dim[-1], decoder_embed_dim, bias=True)
 
